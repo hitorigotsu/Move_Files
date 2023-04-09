fcbb1afab9f1e766f04efd9f44fc22ff161b36b2
hitorigotsu <129238559+hitorigotsu@users.noreply.github.com>
2023-04-09 11:33:34 +0900

    Update Move_Files.ps1

--- Move_Files.ps1
+++ Move_Files.ps1
@@ -18,7 +18,7 @@ if(Test-Path $Input){
 }
 
 
-#スクリプト実行時点から前日までのファイル一覧を取得
+#前日までのファイル一覧を取得
 $Source = Get-ChildItem $Target_Folder | Where-Object {$_.LastWriteTime.Date -lt $Now_Time -and $_.LastWriteTime.Date -ge $Before_Time}
 #特定の文字列を含むファイルの移動
 $Source | Where-Object {$_.Name -match "^Get"} | Move-Item -Destination $Input
